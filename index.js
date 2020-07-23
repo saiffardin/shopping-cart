7f9125b760066e04238569d47e83340947087a2d
saiffardin <saiffardin@gmail.com>
2020-07-23 15:13:25 +0600

    2nd item done

--- index.js
+++ index.js
@@ -5,13 +5,18 @@
 
 
 const unitPriceIphone = 1219;
-
+const unitPricePhnCover = 59;
 
 
 var idBtnMinus1 = "btn-minus-1";
 var idBtnPlus1 = "btn-plus-1";
 var idTxtInput1 = "input-txt-1";
 var idPriceItem1 = "price-item-1";
+
+var idBtnMinus2 = "btn-minus-2";
+var idBtnPlus2 = "btn-plus-2";
+var idTxtInput2 = "input-txt-2";
+var idPriceItem2 = "price-item-2";
 
 
 
@@ -41,14 +46,8 @@ function updateItemPrice (id,totalPrice) {
     element.innerHTML = totalPrice;
 }
 
-
-
-// ---------------------------------------
-
-// item-1 minus clicked
-getElement(idBtnMinus1).addEventListener('click', function () {
-
-    var quantity = getTxtInput(idTxtInput1);
+function decrement(idTxtInput, idPriceItem, unitPrice) {
+    var quantity = getTxtInput(idTxtInput);
     quantity--;
 
     if (quantity < 0) {
@@ -56,22 +55,44 @@ getElement(idBtnMinus1).addEventListener('click', function () {
         return;
     }
 
-    updateItemAmount(idTxtInput1, quantity);
-    updateItemPrice(idPriceItem1,(unitPriceIphone*quantity));
+    updateItemAmount(idTxtInput, quantity);
+    updateItemPrice(idPriceItem,(unitPrice*quantity));
+}
 
+function increment(idTxtInput, idPriceItem, unitPrice) {
+    var quantity = getTxtInput(idTxtInput);
+    quantity++;
+
+    updateItemAmount(idTxtInput, quantity);
+    updateItemPrice(idPriceItem,(unitPrice*quantity));
+}
+
+
+// ---------------------------------------
+
+// item-1 minus clicked
+getElement(idBtnMinus1).addEventListener('click', function () {
+    decrement(idTxtInput1,idPriceItem1,unitPriceIphone);
 });
-
 
 
 // item-1 plus clicked
 getElement(idBtnPlus1).addEventListener('click', function () {
-
-    var quantity = getTxtInput(idTxtInput1);
-    quantity++;
-
-    updateItemAmount(idTxtInput1, quantity);
-    updateItemPrice(idPriceItem1,(unitPriceIphone*quantity));
-
-
+    increment(idTxtInput1, idPriceItem1,unitPriceIphone);
 });
+
+
+// ---------------------------------------
+
+// item-2 minus clicked
+getElement(idBtnMinus2).addEventListener('click', function () {
+    decrement(idTxtInput2,idPriceItem2,unitPricePhnCover);
+});
+
+
+// item-2 plus clicked
+getElement(idBtnPlus2).addEventListener('click', function () {
+    increment(idTxtInput2, idPriceItem2,unitPricePhnCover);
+});
+
 
